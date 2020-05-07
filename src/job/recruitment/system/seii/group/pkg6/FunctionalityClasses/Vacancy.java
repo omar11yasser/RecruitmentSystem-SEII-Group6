f0ba914568b38cc6f183e5fa97cf4bb5b0dba3df
Omar Yasser <omaryassersalah@outlook.com>
2020-05-07 22:16:08 +0200

    Commit 6: Added setters and getters to the vacancy class.

--- src/job/recruitment/system/seii/group/pkg6/FunctionalityClasses/Vacancy.java
+++ src/job/recruitment/system/seii/group/pkg6/FunctionalityClasses/Vacancy.java
@@ -36,6 +36,80 @@ public class Vacancy {
         this.observersList = observersList;
     }
 
+    public String getVacancyID() {
+        return vacancyID;
+    }
+
+    public void setVacancyID(String vacancyID) {
+        this.vacancyID = vacancyID;
+    }
+
+    public String getVacancyTitle() {
+        return vacancyTitle;
+    }
+
+    public void setVacancyTitle(String vacancyTitle) {
+        this.vacancyTitle = vacancyTitle;
+    }
+
+    public String getVacancyJobRequirments() {
+        return vacancyJobRequirments;
+    }
+
+    public void setVacancyJobRequirments(String vacancyJobRequirments) {
+        this.vacancyJobRequirments = vacancyJobRequirments;
+    }
+
+    public String getVacancyEmployerId() {
+        return vacancyEmployerId;
+    }
+
+    public void setVacancyEmployerId(String vacancyEmployerId) {
+        this.vacancyEmployerId = vacancyEmployerId;
+    }
+
+    public long getDeadlineTimeStamp() {
+        return deadlineTimeStamp;
+    }
+
+    public void setDeadlineTimeStamp(long deadlineTimeStamp) {
+        this.deadlineTimeStamp = deadlineTimeStamp;
+    }
+
+    public long getCreationTimeStamp() {
+        return creationTimeStamp;
+    }
+
+    public void setCreationTimeStamp(long creationTimeStamp) {
+        this.creationTimeStamp = creationTimeStamp;
+    }
+
+    public String getVacancyState() {
+        return vacancyState;
+    }
+
+    public void setVacancyState(String vacancyState) {
+        this.vacancyState = vacancyState;
+    }
+
+    public List<JobApplication> getListOfApplicantions() {
+        return listOfApplicantions;
+    }
+
+    public void setListOfApplicantions(List<JobApplication> listOfApplicantions) {
+        this.listOfApplicantions = listOfApplicantions;
+    }
+
+    public List<Admin> getObserversList() {
+        return observersList;
+    }
+
+    public void setObserversList(List<Admin> observersList) {
+        this.observersList = observersList;
+    }
+    
+    
+
     public List<JobApplication> viewSubmittedApplications(){
         
         return listOfApplicantions;
