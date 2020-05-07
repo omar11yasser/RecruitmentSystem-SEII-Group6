ce487a1a1fb8de4cae1482e314ec6522e2fce414
sawyy <64809596+sawyy@users.noreply.github.com>
2020-05-07 21:56:03 +0200

    basic functions

--- src/job/recruitment/system/seii/group/pkg6/FunctionalityClasses/JobApplication.java
+++ src/job/recruitment/system/seii/group/pkg6/FunctionalityClasses/JobApplication.java
@@ -10,5 +10,68 @@ package job.recruitment.system.seii.group.pkg6.FunctionalityClasses;
  * @author Omar
  */
 public class JobApplication {
-    
+  
+   private String ApplicationId;
+   private String ApllicationAuthourID;
+   private String VacancyID;
+   private String EmployerResponse;
+   private long SubmissionDataTimeStamp;
+   private String UserFilledResponse;
+
+    public JobApplication(String ApplicationId, String ApllicationAuthourID, String VacancyID, String EmployerResponse, long SubmissionDataTimeStamp, String UserFilledResponse) {
+        this.ApplicationId = ApplicationId;
+        this.ApllicationAuthourID = ApllicationAuthourID;
+        this.VacancyID = VacancyID;
+        this.EmployerResponse = EmployerResponse;
+        this.SubmissionDataTimeStamp = SubmissionDataTimeStamp;
+        this.UserFilledResponse = UserFilledResponse;
+    }
+
+    public String getApplicationId() {
+        return ApplicationId;
+    }
+
+    public void setApplicationId(String ApplicationId) {
+        this.ApplicationId = ApplicationId;
+    }
+
+    public String getApllicationAuthourID() {
+        return ApllicationAuthourID;
+    }
+
+    public void setApllicationAuthourID(String ApllicationAuthourID) {
+        this.ApllicationAuthourID = ApllicationAuthourID;
+    }
+
+    public String getVacancyID() {
+        return VacancyID;
+    }
+
+    public void setVacancyID(String VacancyID) {
+        this.VacancyID = VacancyID;
+    }
+
+    public String getEmployerResponse() {
+        return EmployerResponse;
+    }
+
+    public void setEmployerResponse(String EmployerResponse) {
+        this.EmployerResponse = EmployerResponse;
+    }
+
+    public long getSubmissionDataTimeStamp() {
+        return SubmissionDataTimeStamp;
+    }
+
+    public void setSubmissionDataTimeStamp(long SubmissionDataTimeStamp) {
+        this.SubmissionDataTimeStamp = SubmissionDataTimeStamp;
+    }
+
+    public String getUserFilledResponse() {
+        return UserFilledResponse;
+    }
+
+    public void setUserFilledResponse(String UserFilledResponse) {
+        this.UserFilledResponse = UserFilledResponse;
+    }
 }
