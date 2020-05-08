e9a893b6fef484120c160e03f721d13518b5a18a
sawyy <64809596+sawyy@users.noreply.github.com>
2020-05-08 19:14:50 +0200

    new

--- src/job/recruitment/system/seii/group/pkg6/FunctionalityClasses/JobSeeker.java
+++ src/job/recruitment/system/seii/group/pkg6/FunctionalityClasses/JobSeeker.java
@@ -1,9 +1,34 @@
 package job.recruitment.system.seii.group.pkg6.FunctionalityClasses;
 
+import java.util.List;
+
 /**
  *
  * @author Sawy
  */
 public class JobSeeker extends User {
     
+    private List <Vacancy> SavedVacancy;
+    private List <JobApplication> ListOfPastSubmission;
+    private List <Achivment> ListOfAchivment;
+ 
+    
+    
+    
+    void addAchivment(){
+    };
+    void UpdateResume(){
+    };
+    void DeleteAchivment(){
+    };
+    void SaveI_Vacancies(){
+    };
+    void getSavedVacancy(){
+    };
+    void CreateJobApplication(){
+    };
+    void SubmitJobApplication(){
+    };
+    
 }
+
