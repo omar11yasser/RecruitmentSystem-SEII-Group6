19a9c8a481575707b5ff895895880d5522c9bc97
sawyy <64809596+sawyy@users.noreply.github.com>
2020-05-08 21:07:57 +0200

    new

---
+++ src/job/recruitment/system/seii/group/pkg6/Utils/JobApplicationDatabase.java
@@ -0,0 +1,66 @@
+package job.recruitment.system.seii.group.pkg6.Utils;
+import java.sql.*;
+import job.recruitment.system.seii.group.pkg6.FunctionalityClasses.JobApplication;
+/**
+ * @author Omar
+ */
+public class JobApplicationDatabase extends DatabaseConnection {
+    
+    public static void main(String[] args){
+        
+        try {
+            retieveApplication("Application154858");
+        } catch(Exception f){
+            f.printStackTrace();
+        }
+    }
+    
+    
+    public static void insertApplicationIntoDatabase(JobApplication Application) throws SQLException{
+        Connection con = DatabaseConnection.connect();
+        String query = "insert Application(Application_Id , Application_Authour_Id, User_Filled_Response,Vacancy_id,Employer_Response"+ ", Submission_timestamp) values(? , ? , ? ,?, ?,? , ?)";
+        PreparedStatement preparedStatment = con.prepareStatement(query);
+        preparedStatment.setString(1 , Application.getApplicationId());
+        preparedStatment.setString(2 , Application.getApllicationAuthourID());
+        preparedStatment.setString(3, Application.getUserFilledResponse());
+        preparedStatment.setString(4, Application.getVacancyID());
+        preparedStatment.setString(5, Application.getEmployerResponse());
+        preparedStatment.setTimestamp(5, new Timestamp(Application.getSubmissionDataTimeStamp()));
+        
+        preparedStatment.execute();
+        con.close();
+
+
+    }
+    
+    
+    public static JobApplication retieveApplication(String ApplicationId) throws SQLException{
+        Connection con = DatabaseConnection.connect();
+        String query = "SELECT * FROM  WHERE Application_Id = '" + ApplicationId +"'";
+        Statement stmt  = con.createStatement();
+        ResultSet result = stmt.executeQuery(query);
+        JobApplication Application = new JobApplication();
+        
+        // Navigate the result 
+        while (result.next()){
+        Application.setVacancyID(result.getString(1));
+        Application.setVacancyEmployerId(result.getString(2));
+        Application.setVacancyTitle(result.getString(3));
+        Application.setVacancyJobRequirments(result.getString(4));
+        Application.setDeadlineTimeStamp(result.getTimestamp(5).getTime());
+        Application.setCreationTimeStamp(result.getTimestamp(6).getTime());
+        Application.setVacancyState(result.getString(7));
+        }
+        con.close();
+        return Application;
+    }
+
+    
+    
+  
+
+  
+    
+}
+
+    
