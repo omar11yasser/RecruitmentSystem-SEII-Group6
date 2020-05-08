15eb7e62f5c3e2a255d7645a6f19a4eafda6209a
Omar Yasser <omaryassersalah@outlook.com>
2020-05-08 20:21:52 +0200

    Commit 15: 	- Fixed admin errors

--- src/job/recruitment/system/seii/group/pkg6/FunctionalityClasses/Admin.java
+++ src/job/recruitment/system/seii/group/pkg6/FunctionalityClasses/Admin.java
@@ -1,6 +1,9 @@
 package job.recruitment.system.seii.group.pkg6.FunctionalityClasses;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import job.recruitment.system.seii.group.pkg6.Utils.DatabaseConnection;
 
 /**
  *
@@ -14,15 +17,21 @@ public class Admin {
     private String adminPassword;
 
     public static Admin Register(String name, String email, String password){
-        Connection con = DatabaseConnection.connect();
-        String query = "INSERT INTO `admins` (`id`, `name`, `email`, `password`) VALUES (NULL, '"+name+"', '"+email+"', '"+password+"')";
-        Statement stmt  = con.createStatement();
-        ResultSet result = stmt.executeQuery(query);
-        Admin AdminObject = new Admin;
-        AdminObject.adminName = name;
-        AdminObject.adminMail = email;
-        AdminObject.adminPassword = password;
-        return AdminObject;
+        try {
+            Connection con = DatabaseConnection.connect();
+            String query = "INSERT INTO `admins` (`id`, `name`, `email`, `password`) VALUES (NULL, '"+name+"', '"+email+"', '"+password+"')";
+            Statement stmt  = con.createStatement();
+            ResultSet result = stmt.executeQuery(query);
+            Admin AdminObject = new Admin();
+            AdminObject.adminName = name;
+            AdminObject.adminMail = email;
+            AdminObject.adminPassword = password;
+            return AdminObject;
+        } catch (SQLException ex) {
+            Logger.getLogger(Admin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return 
     }
 
     public static Admin Login(String email, String password){
@@ -32,7 +41,7 @@ public class Admin {
         ResultSet result = stmt.executeQuery(query);
 
         if(false/*ResultSet.Lentgh > 0*/) {
-            Admin AdminObject = new Admin;
+            Admin AdminObject = new Admin();
             AdminObject.adminName = "name";
             AdminObject.adminMail = email;
             AdminObject.adminPassword = password;
@@ -48,8 +57,13 @@ public class Admin {
     }
 
     public Vacancy ViewVacancyDetails(String id){
-        Vacancy VacancyObject = new Vacancy;
-        return VacancyObject.retieveFromVacancyUsingVacancyId(id);
+        Vacancy VacancyObject = new Vacancy();
+        try {
+            return VacancyObject.getVacancyUsingId(id);
+        } catch (SQLException ex) {
+            Logger.getLogger(Admin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
 
     public void approveNewEmployers(){
